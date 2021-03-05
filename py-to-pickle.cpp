2d66af38a9fccf2de93f0e7164c7e8921562b42d
Albert Zeyer <albzey@gmail.com>
2021-03-05 15:21:46 +0100

    space to tabs

--- py-to-pickle.cpp
+++ py-to-pickle.cpp
@@ -15,131 +15,131 @@ const int protocol = 3;
 /* Pickle opcodes. These must be kept updated with pickle.py.
    Extensive docs are in pickletools.py. */
 enum opcode {
-    MARK            = '(',
-    STOP            = '.',
-    POP             = '0',
-    POP_MARK        = '1',
-    DUP             = '2',
-    FLOAT           = 'F',
-    INT             = 'I',
-    BININT          = 'J',
-    BININT1         = 'K',
-    LONG            = 'L',
-    BININT2         = 'M',
-    NONE            = 'N',
-    PERSID          = 'P',
-    BINPERSID       = 'Q',
-    REDUCE          = 'R',
-    STRING          = 'S',
-    BINSTRING       = 'T',
-    SHORT_BINSTRING = 'U',
-    UNICODE         = 'V',
-    BINUNICODE      = 'X',
-    APPEND          = 'a',
-    BUILD           = 'b',
-    GLOBAL          = 'c',
-    DICT            = 'd',
-    EMPTY_DICT      = '}',
-    APPENDS         = 'e',
-    GET             = 'g',
-    BINGET          = 'h',
-    INST            = 'i',
-    LONG_BINGET     = 'j',
-    LIST            = 'l',
-    EMPTY_LIST      = ']',
-    OBJ             = 'o',
-    PUT             = 'p',
-    BINPUT          = 'q',
-    LONG_BINPUT     = 'r',
-    SETITEM         = 's',
-    TUPLE           = 't',
-    EMPTY_TUPLE     = ')',
-    SETITEMS        = 'u',
-    BINFLOAT        = 'G',
+	MARK            = '(',
+	STOP            = '.',
+	POP             = '0',
+	POP_MARK        = '1',
+	DUP             = '2',
+	FLOAT           = 'F',
+	INT             = 'I',
+	BININT          = 'J',
+	BININT1         = 'K',
+	LONG            = 'L',
+	BININT2         = 'M',
+	NONE            = 'N',
+	PERSID          = 'P',
+	BINPERSID       = 'Q',
+	REDUCE          = 'R',
+	STRING          = 'S',
+	BINSTRING       = 'T',
+	SHORT_BINSTRING = 'U',
+	UNICODE         = 'V',
+	BINUNICODE      = 'X',
+	APPEND          = 'a',
+	BUILD           = 'b',
+	GLOBAL          = 'c',
+	DICT            = 'd',
+	EMPTY_DICT      = '}',
+	APPENDS         = 'e',
+	GET             = 'g',
+	BINGET          = 'h',
+	INST            = 'i',
+	LONG_BINGET     = 'j',
+	LIST            = 'l',
+	EMPTY_LIST      = ']',
+	OBJ             = 'o',
+	PUT             = 'p',
+	BINPUT          = 'q',
+	LONG_BINPUT     = 'r',
+	SETITEM         = 's',
+	TUPLE           = 't',
+	EMPTY_TUPLE     = ')',
+	SETITEMS        = 'u',
+	BINFLOAT        = 'G',
 
-    /* Protocol 2. */
-    PROTO       = '\x80',
-    NEWOBJ      = '\x81',
-    EXT1        = '\x82',
-    EXT2        = '\x83',
-    EXT4        = '\x84',
-    TUPLE1      = '\x85',
-    TUPLE2      = '\x86',
-    TUPLE3      = '\x87',
-    NEWTRUE     = '\x88',
-    NEWFALSE    = '\x89',
-    LONG1       = '\x8a',
-    LONG4       = '\x8b',
+	/* Protocol 2. */
+	PROTO       = '\x80',
+	NEWOBJ      = '\x81',
+	EXT1        = '\x82',
+	EXT2        = '\x83',
+	EXT4        = '\x84',
+	TUPLE1      = '\x85',
+	TUPLE2      = '\x86',
+	TUPLE3      = '\x87',
+	NEWTRUE     = '\x88',
+	NEWFALSE    = '\x89',
+	LONG1       = '\x8a',
+	LONG4       = '\x8b',
 
-    /* Protocol 3 (Python 3.x) */
-    BINBYTES       = 'B',
-    SHORT_BINBYTES = 'C',
+	/* Protocol 3 (Python 3.x) */
+	BINBYTES       = 'B',
+	SHORT_BINBYTES = 'C',
 
-    /* Protocol 4 */
-    SHORT_BINUNICODE = '\x8c',
-    BINUNICODE8      = '\x8d',
-    BINBYTES8        = '\x8e',
-    EMPTY_SET        = '\x8f',
-    ADDITEMS         = '\x90',
-    FROZENSET        = '\x91',
-    NEWOBJ_EX        = '\x92',
-    STACK_GLOBAL     = '\x93',
-    MEMOIZE          = '\x94',
-    FRAME            = '\x95',
+	/* Protocol 4 */
+	SHORT_BINUNICODE = '\x8c',
+	BINUNICODE8      = '\x8d',
+	BINBYTES8        = '\x8e',
+	EMPTY_SET        = '\x8f',
+	ADDITEMS         = '\x90',
+	FROZENSET        = '\x91',
+	NEWOBJ_EX        = '\x92',
+	STACK_GLOBAL     = '\x93',
+	MEMOIZE          = '\x94',
+	FRAME            = '\x95',
 
-    /* Protocol 5 */
-    BYTEARRAY8       = '\x96',
-    NEXT_BUFFER      = '\x97',
-    READONLY_BUFFER  = '\x98'
+	/* Protocol 5 */
+	BYTEARRAY8       = '\x96',
+	NEXT_BUFFER      = '\x97',
+	READONLY_BUFFER  = '\x98'
 };
 
 static void _PyFloat_Pack8(double x, unsigned char *p, int le) {
-    typedef enum {
-        unset, ieee_big_endian_format, ieee_little_endian_format
-    } float_format_type;
-    static float_format_type double_format = unset;
+	typedef enum {
+		unset, ieee_big_endian_format, ieee_little_endian_format
+	} float_format_type;
+	static float_format_type double_format = unset;
 
 #if __SIZEOF_DOUBLE__ == 8
-    if(double_format == unset) {
-        double x = 9006104071832581.0;
-        if (memcmp(&x, "\x43\x3f\xff\x01\x02\x03\x04\x05", 8) == 0)
-            double_format = ieee_big_endian_format;
-        else if (memcmp(&x, "\x05\x04\x03\x02\x01\xff\x3f\x43", 8) == 0)
-            double_format = ieee_little_endian_format;
-        else {
-            fprintf(stderr, "invalid double format");
-            abort();
-        }
-    }
+	if(double_format == unset) {
+		double x = 9006104071832581.0;
+		if (memcmp(&x, "\x43\x3f\xff\x01\x02\x03\x04\x05", 8) == 0)
+			double_format = ieee_big_endian_format;
+		else if (memcmp(&x, "\x05\x04\x03\x02\x01\xff\x3f\x43", 8) == 0)
+			double_format = ieee_little_endian_format;
+		else {
+			fprintf(stderr, "invalid double format");
+			abort();
+		}
+	}
 #else
 #error invalid __SIZEOF_DOUBLE__
 #endif
 
-    const unsigned char *s = (unsigned char*)&x;
-    int i, incr = 1;
+	const unsigned char *s = (unsigned char*)&x;
+	int i, incr = 1;
 
-    if ((double_format == ieee_little_endian_format && !le)
-        || (double_format == ieee_big_endian_format && le)) {
-        p += 7;
-        incr = -1;
-    }
+	if ((double_format == ieee_little_endian_format && !le)
+		|| (double_format == ieee_big_endian_format && le)) {
+		p += 7;
+		incr = -1;
+	}
 
-    for (i = 0; i < 8; i++) {
-        *p = *s++;
-        p += incr;
-    }
+	for (i = 0; i < 8; i++) {
+		*p = *s++;
+		p += incr;
+	}
 }
 
 static void _write_size64(char *out, size_t value) {
-    size_t i;
-    static_assert(sizeof(size_t) <= 8, "sizeof size_t != 8");
+	size_t i;
+	static_assert(sizeof(size_t) <= 8, "sizeof size_t != 8");
 
-    for (i = 0; i < sizeof(size_t); i++) {
-        out[i] = (unsigned char)((value >> (8 * i)) & 0xff);
-    }
-    for (i = sizeof(size_t); i < 8; i++) {
-        out[i] = 0;
-    }
+	for (i = 0; i < sizeof(size_t); i++) {
+		out[i] = (unsigned char)((value >> (8 * i)) & 0xff);
+	}
+	for (i = sizeof(size_t); i < 8; i++) {
+		out[i] = 0;
+	}
 }
 
 struct Reader {
@@ -463,10 +463,10 @@ public:
 };
 
 int main(int argc, char** argv) {
-    if(argc <= 2) {
-        printf("usage: %s <in-py-file> <out-pickle-file>\n", argv[0]);
-        return -1;
-    }
+	if(argc <= 2) {
+		printf("usage: %s <in-py-file> <out-pickle-file>\n", argv[0]);
+		return -1;
+	}
 
 	FileReader reader(argv[1]);
 	if(!reader.valid()) {
