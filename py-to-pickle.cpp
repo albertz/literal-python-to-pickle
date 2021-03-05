02e82cc022b7929ac9a2e862c1e280be18ed7a22
Albert Zeyer <albzey@gmail.com>
2021-03-05 15:19:19 +0100

    prepare for redesign

--- py-to-pickle.cpp
+++ py-to-pickle.cpp
@@ -142,258 +142,325 @@ static void _write_size64(char *out, size_t value) {
     }
 }
 
-static FILE *in, *out;
-static size_t out_pos = 0;
+struct Reader {
+	virtual ~Reader() {}
+	virtual bool valid() = 0;
+	virtual size_t pos() = 0;
+	virtual int read_next_char() = 0;
+};
 
-static int read_next_char() {
-	return getc_unlocked(in);
-}
+struct FileReader : Reader {
+	FILE* fp;
 
-static void write_char(char c) {
-	putc_unlocked(c, out);
-	++out_pos;
-}
+	FileReader(const char* filename) {
+		fp = fopen(filename, "r");
+		if(fp) flockfile(fp);
+	}
+	virtual ~FileReader() { fclose(fp); };
+	virtual bool valid() { return fp; }
+	virtual size_t pos() { return ftell(fp); }
+	virtual int read_next_char() {
+		return getc_unlocked(fp);
+	}
+};
 
-static void write_data(const char* data, size_t len) {
+struct Writer {
+	virtual ~Writer() {}
+	virtual bool valid() = 0;
+	virtual size_t pos() = 0;
+	virtual void seek(size_t pos) = 0;
+	virtual void write_char(char c) = 0;
+	virtual void write_data(const char* data, size_t len) = 0;
+};
+
+struct FileWriter : Writer {
+	FILE* fp;
+	size_t out_pos;
+	
+	FileWriter(const char* filename) {
+		fp = fopen(filename, "w");
+		if(fp) flockfile(fp);
+	}
+	virtual ~FileWriter() { fclose(fp); }
+	virtual bool valid() { return fp; }
+	virtual size_t pos() { return out_pos; }
+	virtual void seek(size_t pos) {
+		fseek(fp, pos, SEEK_SET);
+		out_pos = pos;
+	}
+	virtual void write_char(char c) {
+		putc_unlocked(c, fp);
+		++out_pos;
+	}
+	virtual void write_data(const char* data, size_t len) {
 #ifdef __linux__
-	fwrite_unlocked(data, len, 1, out);
+		fwrite_unlocked(data, len, 1, fp);
 #else
-	fwrite(data, len, 1, out);
+		fwrite(data, len, 1, fp);
 #endif
-	out_pos += len;
-}
-
-[[noreturn]]
-static void parse_error(const char* ctx, char c) {
-    fprintf(stderr, "parse error: %s: char '%c' in pos %li\n", ctx, c, ftell(in));
-	exit(1);
-}
+		out_pos += len;
+	}
+};
 
 typedef std::pair<int,bool> ParseRes;  // next char + parsed one item or not
 
-static ParseRes parse();
+class Parser {
+	Reader* reader;
+	Writer* writer;
 
-static void parse_list() {
-    write_char(EMPTY_LIST);
-    write_char(MARK);
-    while(true) {
-        ParseRes res = parse();
-        int c = res.first;
-        if(c == ',') continue;
-        else if(c == ']') break;
-        else parse_error("list", c);
-    }
-    write_char(APPENDS);
-}
+public:
+	Parser(Reader* reader_, Writer* writer_) : reader(reader_), writer(writer_) {}
 
-static void parse_dict_or_set() {
-    int c;
-    size_t count = 0;
-    enum {Dict, Set} obj_type = Dict;
-    enum {Key, Value} cur = Key;
-    long start_out_pos = out_pos;
-    auto make_set = [&]{
-        if(count != 1) parse_error("dict after parsing more than one entry", c);
-        cur = Value;
-        obj_type = Set;
-        long cur_pos = out_pos;
-        fseek(out, start_out_pos, SEEK_SET);
-        write_char(EMPTY_SET);
-        fseek(out, cur_pos, SEEK_SET);
-		out_pos = cur_pos;
-    };
-    write_char(EMPTY_DICT);
-    write_char(MARK);
-    while(true) {
-        ParseRes res = parse();
-        c = res.first;
-        if(res.second) ++count;
-        if(c == ',') {
-            if(count == 1) make_set();
-            if(cur == Key) parse_error("dict after parsing key", c);
-            if(obj_type == Dict) cur = Key;
-        }
-        else if(c == ':') {
-            if(cur != Key) parse_error("dict expected key before", c);
-            if(obj_type == Set) parse_error("set", c);
-            cur = Value;
-        }
-        else if(c == '}') {
-            if(count == 1) make_set();
-            break;
-        }
-        else parse_error("dict|set", c);
-    }
-    if(obj_type == Dict && count % 2 != 0) parse_error("dict, uneven count", c);
-    write_char((obj_type == Dict) ? SETITEMS : ADDITEMS);
-}
+private:
+	[[noreturn]]
+	void parse_error(const char* ctx, char c) {
+		fprintf(stderr, "parse error: %s: char '%c' in pos %li\n", ctx, c, reader->pos());
+		exit(1);
+	}
 
-static void parse_str(char quote) {
-    // We expect to already have utf8 here (i.e. input file is utf8).
-    // This can potentially be sped up, by writing early,
-    // and then filling in the size. (We might want to ignore BINUNICODE8.)
-    int c;
-    std::string buf;
-    enum {Direct, EscapeInit, EscapeHex} escape_mode = Direct;
-    int escape_hex_pos = 0;
-    int escape_hex = 0;
-    while(true) {
-        c = read_next_char();
-        if(c < 0) parse_error("str, got EOF", c);
-        if(escape_mode == Direct) {
-            if(c == quote) break;
-            if(c == '\\') escape_mode = EscapeInit;
-            else buf.push_back((char) c);
-        }
-        else if(escape_mode == EscapeInit) {
-            if(c == 'x') {
-                escape_mode = EscapeHex;
-                escape_hex_pos = 0;
-                escape_hex = 0;
-            }
-            else {
-                char c_;
-                if(c == 'r') c_ = '\r';
-                else if(c == 't') c_ = '\t';
-                else if(c == 'n') c_ = '\n';
-                else if(c == '\\' || c == '"' || c == '\'' || c == '\n') c_ = char(c);
-                else parse_error("str escaped", c);
-                buf.push_back(c_);
-                escape_mode = Direct;
-            }
-        }
-        else if(escape_mode == EscapeHex) {
-            int h;
-            if(c >= '0' && c <= '9') h = c - '0';
-            else if(c >= 'a' && c <= 'f') h = c - 'a' + 10;
-            else parse_error("str hex escaped", c);
-            escape_hex *= 16;
-            escape_hex += h;
-            escape_hex_pos++;
-            if(escape_hex_pos == 2) {
-                buf.push_back(char(escape_hex));
-                escape_mode = Direct;
-            }
-        }
-    }
+	int read_next_char() { return reader->read_next_char(); }
+	void write_char(char c) { writer->write_char(c); }
+	void write_data(const char* data, size_t len) { writer->write_data(data, len); }
+	
+public:
+	void full_pass() {
+		start();
+		ParseRes res = parse();
+		if(!res.second)
+			parse_error("root", res.first);
+		end();
+	}
+	
+	void start() {
+		write_char(PROTO);
+		write_char(protocol);
+	}
+	void end() {
+		write_char(STOP);
+	}
 
-    char header[9];
-    int len;
-    size_t size = buf.length();
-    if(size <= 0xff) {
-        header[0] = SHORT_BINUNICODE;
-        header[1] = (unsigned char)(size & 0xff);
-        len = 2;
-    }
-    else if(size <= 0xffffffffUL) {
-        header[0] = BINUNICODE;
-        header[1] = (unsigned char)(size & 0xff);
-        header[2] = (unsigned char)((size >> 8) & 0xff);
-        header[3] = (unsigned char)((size >> 16) & 0xff);
-        header[4] = (unsigned char)((size >> 24) & 0xff);
-        len = 5;
-    }
-    else {
-        header[0] = BINUNICODE8;
-        _write_size64(header + 1, size);
-        len = 9;
-    }
-    write_data(header, len);
-    write_data(buf.data(), size);
-}
+	void parse_list() {
+		write_char(EMPTY_LIST);
+		write_char(MARK);
+		while(true) {
+			ParseRes res = parse();
+			int c = res.first;
+			if(c == ',') continue;
+			else if(c == ']') break;
+			else parse_error("list", c);
+		}
+		write_char(APPENDS);
+	}
 
-static int parse_num(char first) {
-    int c;
-    std::string buf;
-    bool is_float = false;
-    buf.push_back(first);
+	void parse_dict_or_set() {
+		int c;
+		size_t count = 0;
+		enum {Dict, Set} obj_type = Dict;
+		enum {Key, Value} cur = Key;
+		long start_out_pos = writer->pos();
+		auto make_set = [&]{
+			if(count != 1) parse_error("dict after parsing more than one entry", c);
+			cur = Value;
+			obj_type = Set;
+			long cur_pos = writer->pos();
+			writer->seek(start_out_pos);
+			write_char(EMPTY_SET);
+			writer->seek(cur_pos);
+		};
+		write_char(EMPTY_DICT);
+		write_char(MARK);
+		while(true) {
+			ParseRes res = parse();
+			c = res.first;
+			if(res.second) ++count;
+			if(c == ',') {
+				if(count == 1) make_set();
+				if(cur == Key) parse_error("dict after parsing key", c);
+				if(obj_type == Dict) cur = Key;
+			}
+			else if(c == ':') {
+				if(cur != Key) parse_error("dict expected key before", c);
+				if(obj_type == Set) parse_error("set", c);
+				cur = Value;
+			}
+			else if(c == '}') {
+				if(count == 1) make_set();
+				break;
+			}
+			else parse_error("dict|set", c);
+		}
+		if(obj_type == Dict && count % 2 != 0) parse_error("dict, uneven count", c);
+		write_char((obj_type == Dict) ? SETITEMS : ADDITEMS);
+	}
 
-    while(true) {
-        c = read_next_char();
-        if(c < 0) break;
+	void parse_str(char quote) {
+		// We expect to already have utf8 here (i.e. input file is utf8).
+		// This can potentially be sped up, by writing early,
+		// and then filling in the size. (We might want to ignore BINUNICODE8.)
+		int c;
+		std::string buf;
+		enum {Direct, EscapeInit, EscapeHex} escape_mode = Direct;
+		int escape_hex_pos = 0;
+		int escape_hex = 0;
+		while(true) {
+			c = read_next_char();
+			if(c < 0) parse_error("str, got EOF", c);
+			if(escape_mode == Direct) {
+				if(c == quote) break;
+				if(c == '\\') escape_mode = EscapeInit;
+				else buf.push_back((char) c);
+			}
+			else if(escape_mode == EscapeInit) {
+				if(c == 'x') {
+					escape_mode = EscapeHex;
+					escape_hex_pos = 0;
+					escape_hex = 0;
+				}
+				else {
+					char c_;
+					if(c == 'r') c_ = '\r';
+					else if(c == 't') c_ = '\t';
+					else if(c == 'n') c_ = '\n';
+					else if(c == '\\' || c == '"' || c == '\'' || c == '\n') c_ = char(c);
+					else parse_error("str escaped", c);
+					buf.push_back(c_);
+					escape_mode = Direct;
+				}
+			}
+			else if(escape_mode == EscapeHex) {
+				int h;
+				if(c >= '0' && c <= '9') h = c - '0';
+				else if(c >= 'a' && c <= 'f') h = c - 'a' + 10;
+				else parse_error("str hex escaped", c);
+				escape_hex *= 16;
+				escape_hex += h;
+				escape_hex_pos++;
+				if(escape_hex_pos == 2) {
+					buf.push_back(char(escape_hex));
+					escape_mode = Direct;
+				}
+			}
+		}
 
-        if((c >= '0' && c <= '9') || c == '+' || c == '-' || c == '.') {
-            if(c == '.')
-                is_float = true;
-            buf.push_back(c);
-        }
-        else
-            break;
-    }
+		char header[9];
+		int len;
+		size_t size = buf.length();
+		if(size <= 0xff) {
+			header[0] = SHORT_BINUNICODE;
+			header[1] = (unsigned char)(size & 0xff);
+			len = 2;
+		}
+		else if(size <= 0xffffffffUL) {
+			header[0] = BINUNICODE;
+			header[1] = (unsigned char)(size & 0xff);
+			header[2] = (unsigned char)((size >> 8) & 0xff);
+			header[3] = (unsigned char)((size >> 16) & 0xff);
+			header[4] = (unsigned char)((size >> 24) & 0xff);
+			len = 5;
+		}
+		else {
+			header[0] = BINUNICODE8;
+			_write_size64(header + 1, size);
+			len = 9;
+		}
+		write_data(header, len);
+		write_data(buf.data(), size);
+	}
 
-    if(is_float) {
-        double val = std::stod(buf);
-        char pdata[9];
-        pdata[0] = BINFLOAT;
-        _PyFloat_Pack8(val, (unsigned char *)&pdata[1], 0);
-        write_data(pdata, sizeof(pdata));
-    }
-    else {
-        int len;
-        long val = std::stol(buf);
-        char pdata[8];
-        pdata[1] = (unsigned char)(val & 0xff);
-        pdata[2] = (unsigned char)((val >> 8) & 0xff);
-        pdata[3] = (unsigned char)((val >> 16) & 0xff);
-        pdata[4] = (unsigned char)((val >> 24) & 0xff);
+	int parse_num(char first) {
+		int c;
+		std::string buf;
+		bool is_float = false;
+		buf.push_back(first);
 
-        if ((pdata[4] != 0) || (pdata[3] != 0)) {
-            pdata[0] = BININT;
-            len = 5;
-        }
-        else if (pdata[2] != 0) {
-            pdata[0] = BININT2;
-            len = 3;
-        }
-        else {
-            pdata[0] = BININT1;
-            len = 2;
-        }
-        write_data(pdata, len);
-    }
-    return c;
-}
+		while(true) {
+			c = read_next_char();
+			if(c < 0) break;
 
-static ParseRes parse() {
-    int c;
-    bool had_one_item = false;
+			if((c >= '0' && c <= '9') || c == '+' || c == '-' || c == '.') {
+				if(c == '.')
+					is_float = true;
+				buf.push_back(c);
+			}
+			else
+				break;
+		}
 
-    while(true) {
-        c = read_next_char();
-        if(c < 0) break;
+		if(is_float) {
+			double val = std::stod(buf);
+			char pdata[9];
+			pdata[0] = BINFLOAT;
+			_PyFloat_Pack8(val, (unsigned char *)&pdata[1], 0);
+			write_data(pdata, sizeof(pdata));
+		}
+		else {
+			int len;
+			long val = std::stol(buf);
+			char pdata[8];
+			pdata[1] = (unsigned char)(val & 0xff);
+			pdata[2] = (unsigned char)((val >> 8) & 0xff);
+			pdata[3] = (unsigned char)((val >> 16) & 0xff);
+			pdata[4] = (unsigned char)((val >> 24) & 0xff);
 
-        if(isspace(c))
-            continue;
+			if ((pdata[4] != 0) || (pdata[3] != 0)) {
+				pdata[0] = BININT;
+				len = 5;
+			}
+			else if (pdata[2] != 0) {
+				pdata[0] = BININT2;
+				len = 3;
+			}
+			else {
+				pdata[0] = BININT1;
+				len = 2;
+			}
+			write_data(pdata, len);
+		}
+		return c;
+	}
 
-        // (continued strings not yet supported...)
-        if(had_one_item)
-            break;
-        
-        if(c == '\'' || c == '"') {
-            parse_str(c);
-            had_one_item = true;
-        }
-        else if(c == '[') {
-            parse_list();
-            had_one_item = true;
-        }
-        else if(c == '{') {
-            parse_dict_or_set();
-            had_one_item = true;
-        }
-        else if((c >= '0' && c <= '9') || c == '+' || c == '-' || c == '.') {
-            c = parse_num(c);
-            had_one_item = true;
-            if(c < 0 || !isspace(c))
-                break;
-        }
-        else
-            // some unexpected char
-            break;
-    }
+	ParseRes parse() {
+		int c;
+		bool had_one_item = false;
 
-    return ParseRes(c, had_one_item);
-}
+		while(true) {
+			c = read_next_char();
+			if(c < 0) break;
+
+			if(isspace(c))
+				continue;
+
+			// (continued strings not yet supported...)
+			if(had_one_item)
+				break;
+			
+			if(c == '\'' || c == '"') {
+				parse_str(c);
+				had_one_item = true;
+			}
+			else if(c == '[') {
+				parse_list();
+				had_one_item = true;
+			}
+			else if(c == '{') {
+				parse_dict_or_set();
+				had_one_item = true;
+			}
+			else if((c >= '0' && c <= '9') || c == '+' || c == '-' || c == '.') {
+				c = parse_num(c);
+				had_one_item = true;
+				if(c < 0 || !isspace(c))
+					break;
+			}
+			else
+				// some unexpected char
+				break;
+		}
+
+		return ParseRes(c, had_one_item);
+	}
+	
+};
 
 int main(int argc, char** argv) {
     if(argc <= 2) {
@@ -401,33 +468,18 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    in = fopen(argv[1], "r");
-	if(!in) {
+	FileReader reader(argv[1]);
+	if(!reader.valid()) {
 		fprintf(stderr, "cannot open input file %s\n", argv[1]);
 		return 1;
 	}
 	
-	out = fopen(argv[2], "w");
-	if(!out) {
+	FileWriter writer(argv[2]);
+	if(!writer.valid()) {
 		fprintf(stderr, "cannot open output file %s\n", argv[2]);
 		return 1;
 	}
 	
-	flockfile(in);
-	flockfile(out);
-
-    write_char(PROTO);
-    write_char(protocol);
-
-    ParseRes res = parse();
-    if(!res.second) {
-        fprintf(stderr, "invalid char %c\n", res.first);
-        abort();
-    }
-    // write_char(NONE);
-
-    write_char(STOP);
-
-    fclose(out);
-    fclose(in);
+	Parser parser(&reader, &writer);
+	parser.full_pass();
 }
